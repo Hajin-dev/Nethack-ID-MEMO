5cad7cda41e20b7c354b46d5bc6aed69cfb65d7e
Hajin-dev <hajin1254@protonmail.com>
2022-04-12 15:19:02 +0900

    Updates

---
+++ precache-manifest.8894577487e997ffe379927199b1f534.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5a6eacc42d4c9a4ea7",
+    "url": "/static/css/main.6355378c.chunk.css"
+  },
+  {
+    "revision": "2e5a6eacc42d4c9a4ea7",
+    "url": "/static/js/main.2e5a6eac.chunk.js"
+  },
+  {
+    "revision": "71860930b774aaeba025",
+    "url": "/static/css/1.9746d8ae.chunk.css"
+  },
+  {
+    "revision": "71860930b774aaeba025",
+    "url": "/static/js/1.71860930.chunk.js"
+  },
+  {
+    "revision": "2b8cfcd329e0c78b1398",
+    "url": "/static/js/2.2b8cfcd3.chunk.js"
+  },
+  {
+    "revision": "9f126da057a3f1e85dc6",
+    "url": "/static/js/3.9f126da0.chunk.js"
+  },
+  {
+    "revision": "e7eaf3c375dd9b046e3e",
+    "url": "/static/js/4.e7eaf3c3.chunk.js"
+  },
+  {
+    "revision": "292714e0a77a6dfa226a",
+    "url": "/static/js/runtime~main.292714e0.js"
+  },
+  {
+    "revision": "2e0b8660b87034ecf56aa9f488fbc08e",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.2e0b8660.woff2"
+  },
+  {
+    "revision": "5cff07beab63ec777fc73ac0483811b0",
+    "url": "/static/media/roboto-greek-ext-400-normal.5cff07be.woff2"
+  },
+  {
+    "revision": "ba2c6cb0af81f8da49a960db84f26b7c",
+    "url": "/static/media/roboto-cyrillic-400-normal.ba2c6cb0.woff2"
+  },
+  {
+    "revision": "73f26bf98a715ecab4d2287ff3a02ad0",
+    "url": "/static/media/roboto-all-400-normal.73f26bf9.woff"
+  },
+  {
+    "revision": "22786f243202d7912399ffd10c76fe78",
+    "url": "/static/media/roboto-greek-400-normal.22786f24.woff2"
+  },
+  {
+    "revision": "756af8e5560d200e53b52b1ff70f2ad0",
+    "url": "/static/media/roboto-vietnamese-400-normal.756af8e5.woff2"
+  },
+  {
+    "revision": "718dded3393324e992b225ac61329e0c",
+    "url": "/static/media/roboto-latin-ext-400-normal.718dded3.woff2"
+  },
+  {
+    "revision": "aa23b7b4bcf2b8f0e876106bb3de69c6",
+    "url": "/static/media/roboto-latin-400-normal.aa23b7b4.woff2"
+  },
+  {
+    "revision": "d880c5ae749558c291f2623918ae24b9",
+    "url": "/index.html"
+  }
+];
